cc505690892c3bf1eefb1fc83243635a72aa09fd
Aleksandra Matiyev <aleksa000777@mail.ru>
2018-10-10 17:19:34 -0700

    update licence

--- umd/react-record.js
+++ umd/react-record.js
@@ -1,6 +1,6 @@
 /*!
- * react-record v1.0.2 - https://github.com/aleksa000777/React-record#readme
- * ISC Licensed
+ * react-record v1.0.3 - https://github.com/aleksa000777/React-record#readme
+ * MIT Licensed
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
